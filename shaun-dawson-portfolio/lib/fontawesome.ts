ef927da7be5af79c00b3d660f0f9079af46ea243
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2025-12-11 20:36:42 -0500

    Track lib/fontawesome.ts

---
+++ shaun-dawson-portfolio/lib/fontawesome.ts
@@ -0,0 +1,5 @@
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+// Tell Font Awesome to skip adding the CSS automatically
+config.autoAddCss = false
