4dce64ade0bc1ee702413ca4e8f42df0dd68484a
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2025-12-15 16:27:01 -0500

    theme update

--- shaun-dawson-portfolio/app/Providers.tsx
+++ shaun-dawson-portfolio/app/Providers.tsx
@@ -7,7 +7,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <FpjsProvider
             loadOptions={{
-                apiKey: 'ikoCglMSg7f74O7QVNgN', // your public API key
+                apiKey: 'ikoCglMSg7f74O7QVNgN',
                 region: 'us',
             }}
         >
