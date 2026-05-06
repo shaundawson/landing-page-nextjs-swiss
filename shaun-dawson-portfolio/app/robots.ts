ddc4f79018a0d7b2b989c40469511f7a29106c77
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2026-05-06 18:46:13 -0400

    Update robots.ts

--- shaun-dawson-portfolio/app/robots.ts
+++ shaun-dawson-portfolio/app/robots.ts
@@ -1,14 +1,13 @@
-import { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next'
+
+export const dynamic = 'force-static'
 
 export default function robots(): MetadataRoute.Robots {
     return {
-        rules: [
-            {
-                userAgent: '*',
-                allow: '/',
-            },
-        ],
+        rules: {
+            userAgent: '*',
+            allow: '/',
+        },
         sitemap: 'https://iamsdawson.com/sitemap.xml',
-        host: 'https://iamsdawson.com',
     }
 }
