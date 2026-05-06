63174c28930d2952876a4f236b1e46986858d508
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2026-05-06 18:56:03 -0400

    Update page.tsx

--- shaun-dawson-portfolio/app/meet/page.tsx
+++ shaun-dawson-portfolio/app/meet/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import MeetingsClient from './MeetingsClient'
 
-export const meta Metadata = {
+export const meta: Metadata = {
   robots: {
     index: false,
     follow: false,
